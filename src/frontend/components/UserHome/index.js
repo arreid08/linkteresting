73123267770023d4244be766766ab0d0a320b689
KirstenForester <kirsten4ester@gmail.com>
2020-01-27 16:13:21 -0600

    UserHome edits

--- src/frontend/components/UserHome/index.js
+++ src/frontend/components/UserHome/index.js
@@ -1,19 +1,50 @@
 import React from 'react'
-
+import Collection from './Collection'
 import './UserHome.css'
 
 function UserHome() {
 
+  const [user, setUser] = useState([])
 
+  useEffect(() => {
+    fetchUserInfo()
+  }, [])
+
+  const fetchUserInfo = () => {
+    fetch('http://localhost:3000/')
+      .then(res => res.json())
+      .then(res => {
+        setUser(res)
+      }, [])
+      .catch((error) => {
+        console.log("error", error)
+      })
+  }
 
   return (
-    <div className="Header">
-          <h1 className="text-center mb-4">{UserName} Collections</h1>
-            <div className="row">
-              <div className="col-lg">{Collection}</div>
-              <div className="col-lg">{Collection}</div>
-              <div className="col-lg">{Collection}</div>
-              <div className="col-lg">{Collection}</div>
+    <div class="Header">
+          <h1 class="text-center mb-4">{UserName} Collections</h1>
+          <div class="container my-container">
+            <div class="row my-row">
+              <div class="col-lg my-col">{Collection}</div>
+              <div class="col-lg">{Collection}</div>
+              <div class="col-lg">{Collection}</div>
+              <div class="col-lg">{Collection}</div>
+            </div>
+
+            <div class="row my-row">
+              <div class="col-lg">{Collection}</div>
+              <div class="col-lg">{Collection}</div>
+              <div class="col-lg">{Collection}</div>
+              <div class="col-lg">{Collection}</div>
+            </div>
+
+            <div class="row my-row">
+              <div class="col-lg">{Collection}</div>
+              <div class="col-lg">{Collection}</div>
+              <div class="col-lg">{Collection}</div>
+              <div class="col-lg">{Collection}</div>
+            </div>
           </div>
     </div>
   )
