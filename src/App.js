e18e49222ee53a67fe0df63ae3129948d46dfad8
Amy Reid <ReidAmyR@JohnDeere.com>
2020-01-27 10:05:12 -0600

    Updated App and Header files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import './App.css'
+import Header from './frontend/components/Header'
+import Login from './frontend/components/Login'
+// import Edit from './frontend/components/Edit'
+// import Add from './frontend/components/Add'
+// import Delete from './frontend/components/Delete'
 
 function App() {
   return (
     <div className="App">
-      <h1>Linkteresting</h1>
+      <Header />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Login />
+          </Route>
+        </Switch>
+      </main>
     </div>
   )
 }
---
+++ src/frontend/components/Login/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Login.css'
+
+function Login() {
+  return (
+    <div className="Welcome">
+      <h1>Login</h1>
+      <div>Please Login</div>
+    </div>
+  )
+}
+
+export default Login
