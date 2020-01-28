648eb05acb588be00d1239270c28ea4ba1c6ea1a
KirstenForester <kirsten4ester@gmail.com>
2020-01-28 08:38:49 -0600

    cont UserHome updates

--- src/frontend/components/UserHome/index.js
+++ src/frontend/components/UserHome/index.js
@@ -1,49 +1,49 @@
 import React from 'react'
-import Collection from './Collection'
+// import Collection from './Collection'
 import './UserHome.css'
 
 function UserHome() {
 
-  const [user, setUser] = useState([])
+  // const [user, setUser] = useState([])
 
-  useEffect(() => {
-    fetchUserInfo()
-  }, [])
+  // useEffect(() => {
+  //   fetchUserInfo()
+  // }, [])
 
-  const fetchUserInfo = () => {
-    fetch('http://localhost:3000/')
-      .then(res => res.json())
-      .then(res => {
-        setUser(res)
-      }, [])
-      .catch((error) => {
-        console.log("error", error)
-      })
-  }
+  // const fetchUserInfo = () => {
+  //   fetch('http://localhost:3000/')
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       setUser(res)
+  //     }, [])
+  //     .catch((error) => {
+  //       console.log("error", error)
+  //     })
+  // }
 
   return (
     <div class="Header">
-          <h1 class="text-center mb-4">{UserName} Collections</h1>
+          <h1 class="text-center mb-4">Username Collections</h1>
           <div class="container my-container">
             <div class="row my-row">
-              <div class="col-lg my-col">{Collection}</div>
-              <div class="col-lg">{Collection}</div>
-              <div class="col-lg">{Collection}</div>
-              <div class="col-lg">{Collection}</div>
+              <div class="col-lg my-col">Collection</div>
+              <div class="col-lg">Collection</div>
+              <div class="col-lg">Collection</div>
+              <div class="col-lg">Collection</div>
             </div>
 
             <div class="row my-row">
-              <div class="col-lg">{Collection}</div>
-              <div class="col-lg">{Collection}</div>
-              <div class="col-lg">{Collection}</div>
-              <div class="col-lg">{Collection}</div>
+              <div class="col-lg">Collection</div>
+              <div class="col-lg">Collection</div>
+              <div class="col-lg">Collection</div>
+              <div class="col-lg">Collection</div>
             </div>
 
             <div class="row my-row">
-              <div class="col-lg">{Collection}</div>
-              <div class="col-lg">{Collection}</div>
-              <div class="col-lg">{Collection}</div>
-              <div class="col-lg">{Collection}</div>
+              <div class="col-lg">Collection</div>
+              <div class="col-lg">Collection</div>
+              <div class="col-lg">Collection</div>
+              <div class="col-lg">Collection</div>
             </div>
           </div>
     </div>
