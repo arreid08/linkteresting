e30b2aa5d0ba82f5ce9e694202ab114c80ab9e3e
KirstenForester <kirsten4ester@gmail.com>
2020-01-28 12:35:59 -0600

    UH buttons + initial styling

--- src/frontend/components/UserHome/index.js
+++ src/frontend/components/UserHome/index.js
@@ -1,18 +1,16 @@
 import React from 'react'
-// import Collection from './Collection'
 import './UserHome.css'
 import { Link } from 'react-router-dom';
 
 function UserHome() {
-  // const { user } = props;
-  // const [user, setUser] = useState([])
+  // const [user] = useState([])
 
   // useEffect(() => {
   //   fetchUserInfo()
   // }, [])
 
   // const fetchUserInfo = () => {
-  //   fetch('http://localhost:3000/')
+  //   fetch('http://localhost:3000/collection')
   //     .then(res => res.json())
   //     .then(res => {
   //       setUser(res)
@@ -24,26 +22,46 @@ function UserHome() {
 
   return (
     <>
-    
+    {/* // map here, 
+    // another return that will return this div
+    // movie.title
+
+------
+- do i have the click leading to the right spot.. "/collection-details"
+   */}
+
     <div className="col-md-6">
+
         <div className="card mb-4 shadow-sm">
-            <div className="card-body">
+
+        <Link to= '/collection-details' className="card-body">
                 <h2> 
-                    UserName Collections
+                    *Collection Title*
                 </h2>
                 <h5>  </h5>
-                <p className="card-text">
-                    Users
-                </p>
-                <Link to= '/collection' className="btn btn-secondary btn-xs btn-block">
-                    Collection
-                </Link>   
-            </div> 
+                <h4 className="card-text">
+                    *Collection Description*
+                </h4>
+
+            </Link> 
         </div>
     </div>
-                <Link to= '/delete-collection' className="btn btn-secondary btn-xs btn-block">
+                <Link to= '/delete-collection' className="btn btn-dark btn-md mb-5">
                   Delete A Collection
                 </Link>
+
+{/*     AMY:
+            <div className="UserHome">
+                  {props.collection.map(collection => {
+                return(
+                  <div className="collection">
+                    <div className="title">{props.collection.title}</div>
+                    <div className="description">{props.collection.description}</div>
+               </div>
+                )
+                })}
+            </div> */}
+
     </>
 )
 }
