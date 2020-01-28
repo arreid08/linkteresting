05b51cfbb54fba4d9d1e1b2da50098f3dc31757b
Jamrod <myfavoritenoisemaker@gmail.com>
2020-01-28 15:34:59 -0700

    pre pull of UserHome

--- src/frontend/components/UserHome/index.js
+++ src/frontend/components/UserHome/index.js
@@ -5,50 +5,72 @@ import { Link } from 'react-router-dom';
 
 function UserHome(props) {
   console.log('userhome', props);
-  // const [user] = useState([])
+  //const [user] = useState([])
 
-  // useEffect(() => {
-  //   fetchUserInfo()
-  // }, [])
 
-  // const fetchUserInfo = () => {
-  //   fetch('http://localhost:3000/collection')
-  //     .then(res => res.json())
-  //     .then(res => {
-  //       setUser(res)
-  //     }, [])
-  //     .catch((error) => {
-  //       console.log("error", error)
-  //     })
-  // }
+  const fetchCollection = (id) => {
+    fetch(`http://localhost:3000/collection/${id}`)
+      .then(res => res.json())
+      .then(res => {
+        return (res)
+      }, [])
+      .catch((error) => {
+        console.log("error", error)
+      })
+  }
+
+  const collections = []
+
+  props.props.location.userDetails.collections.forEach(col => {
+    fetchCollection(col).then(col => {
+      collections.push(col)
+    })
+  })
 
   return (
     <>
-    {/* // map here, 
+      {/* // map here, 
     // another return that will return this div
    */}
 
-    <div className="col-md-6">
+      <div className="col-md-6">
 
         <div className="card mb-4 shadow-sm">
+          {if (collections.length > 0) {
 
-        <Link to= '/collection-details' className="card-body">
-                <h2> 
-                    *Collection Title*
-                </h2>
-                <h5>  </h5>
-                <h4 className="card-text">
-                    *Collection Description*
+          }}
+          {collections.map(col => {
+
+            <Link to='/collection-details' className="card-body">
+              <h2>
+                {col.title}
+              </h2>
+              <h5>  </h5>
+              <h4 className="card-text">
+                {col.description}
+              </h4>
+              <Link to={`/delete-collection/${col._id}`} className="btn btn-dark btn-md mb-5">
+                Delete A Collection
+                </Link>
+            </Link>
+          })}
+          <Link to='/collection-details' className="card-body">
+            <h2>
+              {props.location.userDetails.title}
+            </h2>
+            <h5>  </h5>
+            <h4 className="card-text">
+              *Collection Description*
                 </h4>
-                {/* <Link to={`/delete-collection/${props.location.userDetails.collections._id}`} className="btn btn-dark btn-md mb-5">
-                  Delete A Collection
-                </Link> */}
-            </Link> 
+            <Link to={`/delete-collection/${props.location.userDetails.collections._id}`} className="btn btn-dark btn-md mb-5">
+              Delete A Collection
+                </Link>
+          </Link>
         </div>
-    </div>
+      </div>
 
 
-{/*     TO INCORPORATE:
+      {/*     TO INCORPORATE:
             <div className="UserHome">
                   {props.collection.map(collection => {
                 return(
@@ -60,7 +82,7 @@ function UserHome(props) {
                 })}
             </div> */}
     </>
-)
+  )
 }
 
 export default UserHome
