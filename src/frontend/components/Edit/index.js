ccdd88f174d85f5b8da9ef778d3ccd05178b0d93
KirstenForester <kirsten4ester@gmail.com>
2020-01-28 16:48:19 -0600

    initial editpage

--- src/frontend/components/Edit/index.js
+++ src/frontend/components/Edit/index.js
@@ -1,13 +1,52 @@
 import React from 'react'
-
+import React, { useState, useEffect } from 'react' 
 import './Edit.css'
 
-function Edit() {
+// reference AddLink 
+function Edit(props) {
+  const [collection, editCollection] = useState([])
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+  
+      fetch(`http://list-links.herokuapp.com/api/collection/${props.collectionId}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      })
+      .then((res) => res.json())
+      .then((res) => {
+          editCollection(res)
+          console.log('res', res)
+      }, [])
+      .catch((error) => {
+          console.log("error", error)
+      })
+}
+
   return (
-    <div className="Delete">
-          <h1>Hello</h1>
+    <div className='Edit'>
+        <p>Edit Collection</p>
+        <div className=".btn-group-justified">
+            <button 
+                className="button" 
+                type="submit" 
+                value="Submit"
+                onClick={handleSubmit}
+            >Submit
+            </button>
+            <Link to='/user-home'>
+                <button
+                    className="button" 
+                    type="submit" 
+                    value="Cancel"
+                    >Cancel
+                </button>
+            </Link>
+        </div>
     </div>
-  )
+)
 }
 
 export default Edit
