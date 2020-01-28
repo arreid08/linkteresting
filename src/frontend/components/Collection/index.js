7d3b97deb83c874eb004a2c4f068dd614d40dfc4
Amy Reid <ReidAmyR@JohnDeere.com>
2020-01-28 14:33:17 -0600

    login successful

--- src/frontend/components/Collection/index.js
+++ src/frontend/components/Collection/index.js
@@ -12,7 +12,7 @@ function Collection() {
     }, [])
   
     const fetchAllLinks = () => {
-        fetch('list-links.herokuapp.com/api/link')
+        fetch('http://list-links.herokuapp.com/api/link')
             .then(res => res.json())
             .then(res => {
                 setLinks(res)
@@ -22,36 +22,38 @@ function Collection() {
             })
     }
 
-  return (
-    <div>
-        <h4>Collection Name Here</h4>
-        <nav className="btn-group btn-group-lg">
-            <Link to={{
-                    pathname: '/add-link',
-                    links: links,
-                    fetchAllLinks: {fetchAllLinks}
-                    }}>
-                <button>New Link</button>
-            </Link>
-            <Link to="/user-home">
-                <button>Go Back</button>
-            </Link>
-        </nav>
-        <div className="container">
-            <div className="row">
-                <div className="col-sm-4">
-                    <li><a href="#">Links here..</a></li>
-                </div>
-                <div className="col-sm-4">
-                    <li><a href="#">Links here..</a></li>
-                </div>
-                <div className="col-sm-4">
-                    <li><a href="#">Links here..</a></li>
+    console.log(links)
+
+    return (
+        <div>
+            <h4>Collection Name Here</h4>
+            <nav className="btn-group btn-group-lg">
+                <Link to={{
+                        pathname: '/add-link',
+                        links: links,
+                        fetchAllLinks: {fetchAllLinks}
+                        }}>
+                    <button>New Link</button>
+                </Link>
+                <Link to="/user-home">
+                    <button>Go Back</button>
+                </Link>
+            </nav>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-4">
+                        <li><a href="#">{links.title}</a></li>
+                    </div>
+                    <div className="col-sm-4">
+                        <li><a href="#">Links here..</a></li>
+                    </div>
+                    <div className="col-sm-4">
+                        <li><a href="#">Links here..</a></li>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Collection
