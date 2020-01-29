3b52d93ea47176df253da9544f33039ea92ed172
Jamrod <myfavoritenoisemaker@gmail.com>
2020-01-29 14:26:30 -0700

    editing delete functions

--- src/frontend/components/DeleteCollection/index.js
+++ src/frontend/components/DeleteCollection/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react' 
+import React, { useState, useEffect } from 'react'
 import './DeleteCollection.css'
 import { Link, Redirect } from 'react-router-dom'
 
@@ -7,49 +7,50 @@ function DeleteCollection(props) {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-    
-        fetch(`http://list-links.herokuapp.com/api/collection/${props.collectionId}`, {
-          method: 'DELETE',
-          headers: {
-              'Content-Type': 'application/json'
-          }
-        })
-        .then((res) => res.json())
-        .then((res) => {
-            setCollection(res)
-            console.log('res', res)
-        }, [])
-        .catch((error) => {
-            console.log("error", error)
-        })
-}
 
-        //   return(
-        //     <Redirect push to='/user-home' />
-        //   )
-    
+        fetch(`http://list-links.herokuapp.com/api/collection/${props.collectionId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                setCollection(res)
+                console.log('res', res)
+            }, [])
+            .catch((error) => {
+                console.log("error", error)
+            })
+
+    }
+
+    //   return(
+    //     <Redirect push to='/user-home' />
+    //   )
+
     return (
         <div className='Delete'>
             <p>Are you sure you want to delete this collection?</p>
             <div className=".btn-group-justified">
-                <button 
-                    className="button" 
-                    type="submit" 
+                <button
+                    className="button"
+                    type="submit"
                     value="Submit"
                     onClick={handleSubmit}
                 >Submit
                 </button>
                 <Link to='/user-home'>
                     <button
-                        className="button" 
-                        type="submit" 
+                        className="button"
+                        type="submit"
                         value="Cancel"
-                        >Cancel
+                    >Cancel
                     </button>
                 </Link>
             </div>
         </div>
     )
 }
-    
+
 export default DeleteCollection
