471846d7d21e2de4a02555bf9fc21ae3a06da345
Amy Reid <ReidAmyR@JohnDeere.com>
2020-01-30 15:27:18 -0600

    Merging

--- src/frontend/components/Collection/index.js
+++ src/frontend/components/Collection/index.js
@@ -6,7 +6,6 @@ import './Collection.css'
 
 function Collection(props) {
 
-    console.log("collection props", props)
     const [links, setLinks] = useState([])
     const [collection, setCollection] = useState([])
 
@@ -17,7 +16,6 @@ function Collection(props) {
                 setCollection(props.state.location.collection)
                 props.getLinkList(props.state.location.id)
                     .then(res => {
-                        console.log("Setting Links ", res)
                         setLinks(res)
                     })
             } else {
@@ -28,9 +26,6 @@ function Collection(props) {
         }
 
     }, [])
-
-    console.log("links", links)
-    console.log("collection", collection)
 
     return (
         <div>
@@ -54,14 +49,13 @@ function Collection(props) {
                     <div className="col-sm-4">
                         <h6>Collection: {collection.description}</h6>
                         <div className="btn-group">
-
-                            {console.log("This is links", links)}
                             {links.map((item) => (
                                 <div className="link-list">
                                     <a href={item.link} target="blank">{item.title}</a>
                                     <Link to={{
                                         pathname: "/edit-link",
-                                        id: item._id
+                                        linkId: item._id,
+                                        collectionId: collection._id
                                     }}>Edit</Link>
                                     <Link to={{
                                         pathname: "/delete-link",
--- src/frontend/components/Edit/index.js
+++ src/frontend/components/Edit/index.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from 'react' 
+import React, { useState } from 'react' 
 import './Edit.css'
 import { Link, Redirect } from 'react-router-dom'
 
 function Edit(props) {
+
+  console.log("edit props", props)
+  console.log("edit state", props.state)
+  console.log("edit location", props.state.location.linkId)
 
   const [title, setTitle] = useState([])
   const [link, setLink] = useState([])
@@ -24,7 +28,7 @@ function Edit(props) {
       link: link
     }
 
-    fetch(`http://list-links.herokuapp.com/api/link/${props.state.location.collectionId}`, {
+    fetch(`http://list-links.herokuapp.com/api/link/${props.state.location.linkId}`, {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json'
@@ -40,12 +44,12 @@ function Edit(props) {
   }
 
   const fetchLinkTitle = (id) => {
-    const foundT = props.links.find(d => d._id === id)
+    const foundT = props.state.location.linkId.find(d => d._id === id)
     return foundT && foundT.title
   }
 
   const fetchLink = (id) => {
-    const foundL = props.links.find(d => d._id === id)
+    const foundL = props.state.location.linkId.find(d => d._id === id)
     return foundL && foundL.link
   }
 
@@ -58,11 +62,11 @@ function Edit(props) {
       <h4>Edit</h4>
       <form className="form" action="/action_page.php" onSubmit={handleSubmit} method="post">
         <label className="label">
-          Title: <input className="text-box" type="text" onChange={handleChangeTitle} defaultValue={fetchLinkTitle(props.state.location.collectionId)} />
+          Title: <input className="text-box" type="text" onChange={handleChangeTitle} defaultValue={fetchLinkTitle(props.state.location.linkId)} />
         </label>
         <br/>
         <label className="label">
-          Link: <input className="text-box" type="text" onChange={handleChangeLink} defaultValue={fetchLink(props.state.location.collectionId)} />
+          Link: <input className="text-box" type="text" onChange={handleChangeLink} defaultValue={fetchLink(props.state.location.linkId)} />
         </label>
         <br/>
         <input className="button" type="submit" value="Submit" />
