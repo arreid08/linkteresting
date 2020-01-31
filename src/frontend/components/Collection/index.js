161b45897bf046f5ac58fd38f053893d6a5819ee
KirstenForester <kirsten4ester@gmail.com>
2020-01-31 08:44:35 -0600

    modifs

--- src/frontend/components/Collection/index.js
+++ src/frontend/components/Collection/index.js
@@ -46,15 +46,15 @@ function Collection(props) {
                     className="btn-group">
                     <button className="btn btn-dark btn-md mb-5">New Link</button>
                 </Link>
-
-
             </nav>
+            
             <div className="container">
-                <div className="row">
-                    <div className="col-sm-4">
+                {/* <div className="row"> */}
+                    <div className="col">
                         <h6>{collection.description}</h6>
                         <div className="btn-group">
                             {links.map((item,i) => (
+
                                 <div className="link-list" key={i}>
                                     <a href={item.link} target="blank">{item.title}</a><br></br>
                                     <Link to={{
@@ -69,12 +69,28 @@ function Collection(props) {
                                         collectionId: collection._id
                                     }}>Delete</Link>
                                 </div>
+                            //     <div className="link-list" key={i}>
+                            //     <a href={item.link} tlirget="blank">{item.title}</a>
+                            //     <br></br>
+                            //     <div className="btn-group">
+                            //     <Link to={{
+                            //         pathname: "/edit-link",
+                            //         linkId: item._id,
+                            //         link: item,
+                            //         collectionId: collection._id
+                            //     }}>Edit</Link>
+                                
+                            //     <Link to={{
+                            //         pathname: "/delete-link",
+                            //         id: item._id,
+                            //         collectionId: collection._id
+                            //     }}>Delete</Link>
+                            //     </div>
+                            // </div>
                             ))}
-
-
                         </div>
                     </div>
-                </div>
+                {/* </div> */}
             </div>
         </div>
     )
